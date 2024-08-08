ec81f3772c0b0212d0938f5a85e07f8dc4a5031e
bhumikamittal7 <bhumika.mittal_ug24@ashoka.edu.in>
2024-08-08 12:02:12 +0200

    minor update

--- cvp2msat.py
+++ cvp2msat.py
@@ -4,7 +4,7 @@ from pysat.formula import WCNF
 # ======================= parameters =======================
 p = 2
 n = 4
-dCap = 999
+dCap = 99
 # ======================= helper functions =======================
 
 def sigma(s):
@@ -35,6 +35,12 @@ def make_clauses(n):
 # print(make_clauses(p,n))
 # print(len(make_clauses(p,n)))
 def inner_product(clause):
+    '''
+    inner product of the basis vectors, b_i and b_j where i and j are the elements of the clause.
+
+    this function is incomplete - need to define the basis vectors and add here
+    returns 1 for now.
+    '''
     return 1
 
 def clause_weight(clause, dCap):
@@ -51,3 +57,9 @@ def clause_weight(clause, dCap):
 # print(clause_weight([-1,-2], dCap))
 
 # ======================= maxsat =======================
+rc2 = RC2(WCNF())  
+for clause in make_clauses(p):
+    rc2.add_clause(clause, weight=clause_weight(clause, dCap))
+
+for model in rc2.enumerate():
+    print(model, rc2.cost)
