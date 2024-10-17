a4b2e1ccf4f538f348427f7f2470259db5366e48
bhumikamittal7 <bhumika.mittal_ug24@ashoka.edu.in>
2024-10-17 10:32:25 +0530

    oct17 changes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pysat.formula import WCNF
 import numpy as np
 import random
 from numpy.linalg import matrix_rank
-
+#================================================================================
 #ignore this for while, let's hardcode the basis for now as an example
 def random_basis(n, m):
     """
@@ -25,20 +25,23 @@ def inner_product(basis, i, j):
     inner product of the basis vectors, b_i and b_j where i and j are the elements of the clause.
     """
     return np.dot(basis[i-1], basis[j-1])
-
+#================================================================================
 def make_clauses_and_weights(basis, n):
     """
     generate SAT clauses and their weights based on the given formula
     """
     k = len(basis)
-    count = n+2     #this is used to introduce new variables in the clauses for y1 <-> x1 & x2. This represents these y variables for each such pair.  
+    #this is used to introduce new variables in the clauses for y1 <-> x1 & x2. This represents these y variables for each such pair. 
+    count = n+2 
     clauses = []
     clauses.append([n+1, 0])  #hard clause
+
     for i in range(1, n+1):
         for j in range(i+1, n+2):
             wi = ((inner_product(basis, i, i) - 2*inner_product(basis, i, k)))
             wj = ((inner_product(basis, j, j) - 2*inner_product(basis, j, k)))
             wk = (wi+wj+2*inner_product(basis, i, j))
+           
             #putting the weight in the last element of the clause
             clauses.append([i, wi])
             if j != n+1:
@@ -52,12 +55,13 @@ def make_clauses_and_weights(basis, n):
             count += 1
     return clauses
 
+#================================================================================
 def print_file(filename, rc2):
     with open(filename, 'w') as file:
         for model in rc2.enumerate():
             file.write(f"Model: {model}, Cost: {rc2.cost}\n")
         file.write(f"Time taken: {rc2.oracle_time()}\n")
-
+#================================================================================
 def run_maxsat(n, basis):
     rc2 = RC2(WCNF())
     
@@ -74,6 +78,7 @@ def run_maxsat(n, basis):
     filename = 'mod_output.txt'
     print_file(filename, rc2)
     return rc2.oracle_time()
+#================================================================================
 
 basis = [[1, -99, 43, -95], [0, 1, 54, -75], [0, 0, 1, 32], [0, 0, 0, 1], [4, -96, 47, -60]]
 print("basis = ", basis)
